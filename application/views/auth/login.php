1709e6e55735f6be83b1e5fc896d687ad0703b97
Irfan <kurniawanirfan2611@gmail.com>
2021-07-13 23:30:57 +0700

    Project Selesai

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -6,6 +6,7 @@
             <div class="col-lg-7">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
+
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
@@ -14,7 +15,7 @@
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
                                     </div>
-
+                                    <?= $this->session->flashdata('message'); ?>
                                     <form class="user" method="post" action="<?= base_url('auth'); ?>">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user" id="email" placeholder="Enter Email Address..." name="email" value="<?= set_value('email'); ?>">
@@ -30,7 +31,7 @@
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        <a class="small" href="<?= base_url('auth/forgotpassword'); ?>">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
                                         <a class="small" href="<?= base_url('auth/registration'); ?>">Create an Account!</a>
